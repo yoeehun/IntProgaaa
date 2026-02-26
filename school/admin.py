b63cb3db3a12fd0d610b7df2c4bdfb4ea75ee516
yoeehun <pantilgan.jonmarco30@gmail.com>
2026-02-26 20:53:38 +0800

    changed db to postgre

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Student, Teacher, Course
 
-# Register your models here.
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Course)
